ba679b68736e95d91691183013b3f8b74974be6e
Vishal-Kushwah <vk16091996@gmail.com>
2020-02-08 01:27:46 +0530

    Final web app on React-js

---
+++ precache-manifest.01424c28b5727b20173807f092b2c8af.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1b6d7e36b7f660064b6bf736c7baaa0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "807b6f6dc878fcbbfa7f",
+    "url": "/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "e885040508171bd5070d",
+    "url": "/static/css/main.cda8f842.chunk.css"
+  },
+  {
+    "revision": "807b6f6dc878fcbbfa7f",
+    "url": "/static/js/2.48f02e3b.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.48f02e3b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e885040508171bd5070d",
+    "url": "/static/js/main.3612ba0e.chunk.js"
+  },
+  {
+    "revision": "2a72882d0811000d450d",
+    "url": "/static/js/runtime-main.10542819.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  }
+]);
